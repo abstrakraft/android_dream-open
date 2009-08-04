2107ce5e6022e2e3db310c1f84755370006264e3
Jean-Baptiste Queru <jbq@google.com>
2009-08-04 13:19:30 -0700

    Alpha-sort lists of proprietary files

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -54,38 +54,39 @@ PRODUCT_COPY_FILES += \
 proprietary := $(LOCAL_PATH)/proprietary
 
 PRODUCT_COPY_FILES += \
-	$(proprietary)/libhtc_ril.so:system/lib/libhtc_ril.so \
+	$(proprietary)/akmd:system/bin/akmd
+
+PRODUCT_COPY_FILES += \
+	$(proprietary)/AudioFilter.csv:system/etc/AudioFilter.csv \
+	$(proprietary)/AudioPara4.csv:system/etc/AudioPara4.csv \
+	$(proprietary)/AudioPreProcess.csv:system/etc/AudioPreProcess.csv \
+	$(proprietary)/gps.conf:system/etc/gps.conf \
+	$(proprietary)/brf6300.bin:system/etc/firmware/brf6300.bin \
+	$(proprietary)/tiwlan.ini:system/etc/wifi/tiwlan.ini \
+	$(proprietary)/Fw1251r1c.bin:system/etc/wifi/Fw1251r1c.bin
+
+PRODUCT_COPY_FILES += \
 	$(proprietary)/libaudioeq.so:system/lib/libaudioeq.so \
-	$(proprietary)/libqcamera.so:system/lib/libqcamera.so \
-	$(proprietary)/libgps.so:obj/lib/libgps.so \
 	$(proprietary)/libgps.so:system/lib/libgps.so \
-	$(proprietary)/libOmxCore.so:obj/lib/libOmxCore.so \
-	$(proprietary)/libOmxCore.so:system/lib/libOmxCore.so \
 	$(proprietary)/libhgl.so:system/lib/libhgl.so \
 	$(proprietary)/libhtc_acoustic.so:system/lib/libhtc_acoustic.so \
+	$(proprietary)/libhtc_ril.so:system/lib/libhtc_ril.so \
 	$(proprietary)/libjni_pinyinime.so:system/lib/libjni_pinyinime.so \
 	$(proprietary)/libmm-adspsvc.so:system/lib/libmm-adspsvc.so \
+	$(proprietary)/libOmxCore.so:system/lib/libOmxCore.so \
 	$(proprietary)/libOmxH264Dec.so:system/lib/libOmxH264Dec.so \
 	$(proprietary)/libOmxMpeg4Dec.so:system/lib/libOmxMpeg4Dec.so \
 	$(proprietary)/libOmxVidEnc.so:system/lib/libOmxVidEnc.so \
 	$(proprietary)/libopencorehw.so:system/lib/libopencorehw.so \
 	$(proprietary)/libpvasf.so:system/lib/libpvasf.so \
 	$(proprietary)/libpvasfreg.so:system/lib/libpvasfreg.so \
-	$(proprietary)/libspeech.so:system/lib/libspeech.so \
+	$(proprietary)/libqcamera.so:system/lib/libqcamera.so \
+	$(proprietary)/libspeech.so:system/lib/libspeech.so
+
+PRODUCT_COPY_FILES += \
 	$(proprietary)/lights.goldfish.so:system/lib/hw/lights.goldfish.so \
-	$(proprietary)/sensors.trout.so:system/lib/hw/sensors.trout.so \
-	$(proprietary)/brf6300.bin:system/etc/firmware/brf6300.bin \
-	$(proprietary)/akmd:system/bin/akmd \
-	$(proprietary)/AudioFilter.csv:system/etc/AudioFilter.csv
-
-PRODUCT_COPY_FILES += \
-	$(proprietary)/AudioPara4.csv:system/etc/AudioPara4.csv \
-	$(proprietary)/gps.conf:system/etc/gps.conf
-
-PRODUCT_COPY_FILES += \
-	$(proprietary)/tiwlan.ini:system/etc/wifi/tiwlan.ini \
-	$(proprietary)/Fw1251r1c.bin:system/etc/wifi/Fw1251r1c.bin
-
-PRODUCT_COPY_FILES += \
 	$(proprietary)/lights.msm7k.so:system/lib/hw/lights.msm7k.so \
-	$(proprietary)/AudioPreProcess.csv:system/etc/AudioPreProcess.csv
+	$(proprietary)/sensors.trout.so:system/lib/hw/sensors.trout.so
+
+PRODUCT_COPY_FILES += \
+	$(proprietary)/libgps.so:obj/lib/libgps.so
